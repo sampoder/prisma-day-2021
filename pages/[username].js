ac9eda8c30163112f71399ad95157a7375763ade
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-06-07 20:17:47 +0800

    Update [username].js

--- pages/[username].js
+++ pages/[username].js
@@ -45,7 +45,7 @@ export default function Page({ preloadSession, initalRedemptions, username }) {
     )
   }
   const fetcher = (...args) => fetch(...args).then(res => res.json())
-  const { data, error } = useSWR(`/api/${username}`, fetcher, {
+  const { data, error } = useSWR(`/api/users/${username}`, fetcher, {
     initialData: initalRedemptions,
   })
   const redemptions = typeof data != 'undefined' ? data : initalRedemptions
